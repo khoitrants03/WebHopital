6eadba6e87b8703cd0b2199d3ddc678771f2c788
PhanThienKhai <117663466+PhanThienKhai@users.noreply.github.com>
2024-10-28 21:21:06 +0700

    Update product.php

--- product.php
+++ product.php
@@ -11,7 +11,7 @@ if (isset($_SESSION['user_id'])) {
 };
 
 include 'components/add_cart.php';
-include './convert_currency.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -59,15 +59,16 @@ include './convert_currency.php';
                <form action="" method="post" class="box">
                   <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
                   <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
-                  <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
+                  <input type="hidden" name="text" value="<?= $fetch_products['text']; ?>">
                   <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
                   <a href="quick_view.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
                   <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
                   <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
                   <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat"><?= $fetch_products['category']; ?></a>
                   <div class="name"><?= $fetch_products['name']; ?></div>
+                  <div class="price"><?= $fetch_products['text']; ?></div>
+
                   <div class="flex">
-                     <div class="price"> <?php echo currency_format($fetch_products['price']); ?></div>
 
                      <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
                   </div>
