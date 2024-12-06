fd17e2181390b3af2ba0513ae9aa975425d626bf
PhanThienKhai <117663466+PhanThienKhai@users.noreply.github.com>
2024-12-06 10:58:22 +0700

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,5 +1,5 @@
 <?php
-$db_name = 'mysql:host=127.0.0.1:4306;dbname=quan_li_bv;charset=utf8mb4';
+$db_name = 'localhost;dbname=quan_li_bv;charset=utf8mb4';
 $user_name = 'root';
 $user_password = '';
 
