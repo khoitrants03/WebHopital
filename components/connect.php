24548c9adcd5ed6bf7dbcfbc83d3494df53b590d
khoitrants03 <trankhoi671@gmail.com>
2024-12-07 10:59:28 +0700

    khoi

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,5 @@
-
 <?php
-$db_name = 'mysql:host=127.0.0.1:3307;dbname=ql_bv;charset=utf8mb4';
-
-// $db_name = 'localhost;dbname=ql_bv;charset=utf8mb4'; code này mở ra để update
+$db_name = 'mysql:host=127.0.0.1:4306;dbname=quan_li_bv;charset=utf8mb4';
 $user_name = 'root';
 $user_password = '';
 
